0685804ad5074e5baf3d4dcb1db0fec256f52ae4
Flint <flint@iMac1.local>
2018-11-26 15:12:06 -0500

    added compare function

--- assignment_nine.py
+++ assignment_nine.py
@@ -21,11 +21,36 @@ def deal_cards(my_deck):
     return user_cards, computer_cards
 
 
+def show_card(user_cards, computer_cards):
+    print("You have a", user_cards)
+    print("The computer has a", computer_cards)
+
+
+def compare(user_cards, computer_cards):
+    user_total = 0
+    computer_total = 0
+    higher_card = user_cards.compare_to(computer_cards)
+    if user_cards == higher_card:
+        print("You win this round!")
+        print("")
+        user_total += 1
+    else:
+        print("The computer wins this round")
+        print("")
+        computer_total += 1
+    return user_total, computer_total
+
+
 def main():
     my_deck = create_deck()
-    deal_cards(my_deck)
-    show_card()
-    compare()
-    winner()
+    user_cards, computer_cards = deal_cards(my_deck)
+    for x in range(len(user_cards)):
+        show_card(user_cards[x], computer_cards[x])
+        user_total, computer_total = compare(user_cards[x], computer_cards[x])
+    if user_total > computer_total:
+        print("You win the game!!!")
+    else:
+        print("The computer wins the game")
+
 
 main()
